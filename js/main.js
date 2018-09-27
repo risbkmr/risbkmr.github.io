9ce8a81369593d9bd8182c2967712cd29ae94e8a
risbkmr <43613554+risbkmr@users.noreply.github.com>
2018-09-27 12:41:59 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ function scrollfun(){
     var wrapper = document.querySelector('.wrapper');
     var themetag = document.querySelector('#themetag');
     var y = wrapper.scrollTop;
-    if(y >= 0 && y < window.innerHeight){
-        themetag.setAttribute('content', '#2fbf71')
+
+    console.log(y)
+    if(y >= 0 && y < window.innerWidth){
+        themetag.setAttribute('content', '#ed143d')
     }
     if(y >= window.innerHeight && y < 2 *window.innerHeight){
         themetag.setAttribute('content', '#2fbf71')
@@ -17,4 +19,5 @@ function scrollfun(){
     if(y >= 4 * window.innerHeight && y < 5 * window.innerHeight){
         themetag.setAttribute('content', '#363537')
     }
+    console.log(themetag.content)
 }
